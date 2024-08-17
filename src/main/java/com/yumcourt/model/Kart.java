2b9a44f7c39edf5da570032f6ebbb442ebf32bc9
Omkar Jadhav <objadhav28@gmail.com>
2024-08-17 08:42:11 +0530

    Commit

---
+++ src/main/java/com/yumcourt/model/Kart.java
@@ -0,0 +1,50 @@
+package com.yumcourt.model;
+
+import java.util.List;
+
+public class Kart {
+    private long id;
+    private double finalPrice;
+    private Customer customer;
+    private Restaurant restaurant;
+
+    public Kart(long id, double finalPrice, Customer customer, Restaurant restaurant) {
+        this.id = id;
+        this.finalPrice = finalPrice;
+        this.customer = customer;
+        this.restaurant = restaurant;
+    }
+
+    // Getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getFinalPrice() {
+        return finalPrice;
+    }
+
+    public void setFinalPrice(double finalPrice) {
+        this.finalPrice = finalPrice;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+}
