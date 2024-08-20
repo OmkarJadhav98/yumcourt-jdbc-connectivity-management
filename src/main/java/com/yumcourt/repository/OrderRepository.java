e8e026acf431d751b48628865a5a98fe96cefa15
Omkar Jadhav <objadhav28@gmail.com>
2024-08-20 11:52:47 +0530

    Commit

--- src/main/java/com/yumcourt/repository/OrderRepository.java
+++ src/main/java/com/yumcourt/repository/OrderRepository.java
@@ -12,7 +12,7 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.project.service.ConnectionService;
+import com.yumcourt.service.ConnectionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
